3c1cbf7d40bba561d3f75a8239f99451147da94f
Spas Genov <spas.genov@gmail.com>
2024-03-05 13:36:45 +0200

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -61,3 +61,34 @@ do
 done
 
 echo "All applications have been installed."
+
+# # Install Oh My Zsh
+# echo "Installing Oh My Zsh..."
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# # Install Powerlevel10k theme for Oh My Zsh
+# echo "Installing Powerlevel10k theme..."
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# # Install zsh-autosuggestions
+# echo "Installing zsh-autosuggestions..."
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# # Install zsh-syntax-highlighting
+# echo "Installing zsh-syntax-highlighting..."
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# # Install fzf-tab
+# echo "Installing fzf-tab..."
+# git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+# # Install fzf
+# echo "Installing fzf..."
+# brew install fzf
+# /usr/local/opt/fzf/install
+
+# # Reminder to update .zshrc
+# echo "Don't forget to update your ~/.zshrc file with the installed plugins and theme."
+
+# Don't forget to add to ~/.zshrc: plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf-tab fzf)
+
